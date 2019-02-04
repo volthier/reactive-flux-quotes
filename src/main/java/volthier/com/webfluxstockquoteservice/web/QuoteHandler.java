0ff19dd861c06aac52c557cb237be8f0664b9733
leonardo <volthier@gmail.com>
2019-02-04 14:38:54 -0200

    [UPDATE] Criado test e Router e Handler

--- src/main/java/volthier/com/webfluxstockquoteservice/web/QuoteHandler.java
+++ src/main/java/volthier/com/webfluxstockquoteservice/web/QuoteHandler.java
@@ -1,7 +1,16 @@
 package volthier.com.webfluxstockquoteservice.web;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import volthier.com.webfluxstockquoteservice.model.Quote;
 import volthier.com.webfluxstockquoteservice.service.QuoteGeneratorService;
+
+import java.time.Duration;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class QuoteHandler {
@@ -12,5 +21,18 @@ public class QuoteHandler {
         this.quoteGeneratorService = quoteGeneratorService;
     }
 
+    public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+        int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100)).take(size), Quote.class);
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+        return ok()
+                .contentType(MediaType.APPLICATION_STREAM_JSON)
+                .body(this.quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(200)), Quote.class);
+    }
 
 }
